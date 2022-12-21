b8150fdb80938503c6c056ad287361646f311c4a
BartN77 <116747040+BartN77@users.noreply.github.com>
2022-12-21 22:42:20 +0100

    22:42 Bart Ruleeeezzzzzz

--- src/main/java/pl/sdacademy/Main.java
+++ src/main/java/pl/sdacademy/Main.java
@@ -1,10 +1,14 @@
 package pl.sdacademy;
 
+
+
 /**
  * @author mszyrner
  */
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+
+    System.out.println("Test_test");
   }
 }
